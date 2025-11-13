b65b9475f01bb254c8f18f3e91c29522093a51bc
Aiden Desjarlais <aiden@aidendes.com>
2025-11-13 13:50:27 -0600

    WIP - New GUI

---
+++ Assets/TESTING/Scripts/NEW_UI/MenuManagerTesting.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System;
+
+public class MenuManagerTesting : MonoBehaviour
+{
+    [SerializeField]
+    private ShipDictionary _shipDictionary;
+
+    [Header("UI Elements")]
+    [SerializeField]
+    private Button _startButton;
+
+    [SerializeField]
+    private Button _shipSelectButton;
+
+    [SerializeField]
+    private Image _shipDisplay;
+
+    [SerializeField]
+    private TMP_Dropdown _gamemodeDropdown;
+
+    [SerializeField]
+    private TextMeshProUGUI _hiScoreText;
+
+    private void OnEnable()
+    {
+        _startButton.onClick.AddListener(StartGame);
+
+        _shipSelectButton.onClick.AddListener(LoadShipSelect);
+
+        Sprite shipSprite = _shipDictionary.Ships[PlayerPrefs.GetInt("ShipSelection", 0)].Sprite;
+
+        _shipDisplay.sprite = shipSprite;
+        _shipDisplay.rectTransform.sizeDelta = new Vector2(shipSprite.textureRect.width * 2, shipSprite.textureRect.height * 2);
+
+        UpdateGamemodeAndHiScore(PlayerPrefs.GetInt("GamemodeSelection", 0));
+
+        _gamemodeDropdown.SetValueWithoutNotify(PlayerPrefs.GetInt("GamemodeSelection", 0));
+        _gamemodeDropdown.onValueChanged.AddListener(UpdateGamemodeAndHiScore);
+    }
+
+    private void OnDisable()
+    {
+        _startButton.onClick.RemoveListener(StartGame);
+
+        _shipSelectButton.onClick.RemoveListener(LoadShipSelect);
+
+        _gamemodeDropdown.onValueChanged.RemoveListener(UpdateGamemodeAndHiScore);
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene("Gameplay_Testing");
+    }
+
+    private void LoadShipSelect()
+    {
+        SceneManager.LoadScene("ShipSelection_Testing");
+    }
+
+    private void UpdateGamemodeAndHiScore(int selection)
+    {
+        PlayerPrefs.SetInt("GamemodeSelection_Testing", selection);
+
+        PlayerPrefs.Save();
+
+        int hiScore = 0;
+
+        switch((Gamemode)selection)
+        {
+            case Gamemode.Standard:
+                hiScore = PlayerPrefs.GetInt("StandardHiScore", 0);
+                break;
+            case Gamemode.Endless:
+                hiScore = PlayerPrefs.GetInt("EndlessHiScore", 0);
+                break;
+            default:
+                break;
+        }
+
+        _hiScoreText.text = hiScore.ToString("00000000");
+    }
+}
