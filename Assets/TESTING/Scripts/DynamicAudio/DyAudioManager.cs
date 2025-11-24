01b68775a4cb7b3afb4634fd936ba6294f80a75a
Aiden Desjarlais <aiden@aidendes.com>
2025-11-24 12:43:45 -0600

    init

---
+++ Assets/TESTING/Scripts/DynamicAudio/DyAudioManager.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DyAudio.Manager
+{
+    public class DyAudioManager : MonoBehaviour
+    {
+        public List<AudioClip> AllAudio = new();
+
+        void Awake()
+        {
+
+        }
+    }
+}
