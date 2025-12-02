8438f39ef7b7e5195a07732b2d2a0f59f297bdb3
Aiden C. Desjarlais <aiden@aidendes.com>
2025-12-02 06:53:46 -0600

    Forgot to add namespace oops

--- Assets/TESTING/Scripts/DynamicAudio/AudioSubsystems/AudioEventDispatcher.cs
+++ Assets/TESTING/Scripts/DynamicAudio/AudioSubsystems/AudioEventDispatcher.cs
@@ -6,59 +6,61 @@ using Audio.Subsystems;
 /*
  These events are dispatched by AudioEventCallers (e.g., CustomAudioClip) to notify subscribers.
  */
-
-[Serializable]
-public class AudioEventDispatcher
+namespace Audio
 {
-    private AudioEventRack parentRack;
-
-    public event Action<CustomAudioClip> ClipBeginPlay;
-    public event Action<CustomAudioClip> ClipPlayEnded;
-    //public event Action<CustomAudioClip> ClipTempoIncrease;
-    //public event Action<CustomAudioClip> ClipTempoDecrease;
-    //public event Action<CustomAudioClip> ClipCustomTime;
-
-    public AudioEventDispatcher(AudioEventRack rack)
+    [Serializable]
+    public class AudioEventDispatcher
     {
-        parentRack = rack;
-    }
+        private AudioEventRack parentRack;
 
-    /// <summary>
-    /// Dispatches an event based on the AudioEventType provided
-    /// </summary>
-    /// <param name="caller">The object calling the event</param>
-    /// <param name="event">The type of event to call</param>
-    public void DispatchEvent(IAudioEventCaller caller, AudioEventType @event)
-    {
-        if (parentRack.Events.Find(e => e.EventType == @event) != null)
+        public event Action<CustomAudioClip> ClipBeginPlay;
+        public event Action<CustomAudioClip> ClipPlayEnded;
+        //public event Action<CustomAudioClip> ClipTempoIncrease;
+        //public event Action<CustomAudioClip> ClipTempoDecrease;
+        //public event Action<CustomAudioClip> ClipCustomTime;
+
+        public AudioEventDispatcher(AudioEventRack rack)
         {
-            CustomAudioClip clip = caller as CustomAudioClip;
-            if (clip != null)
+            parentRack = rack;
+        }
+
+        /// <summary>
+        /// Dispatches an event based on the AudioEventType provided
+        /// </summary>
+        /// <param name="caller">The object calling the event</param>
+        /// <param name="event">The type of event to call</param>
+        public void DispatchEvent(IAudioEventCaller caller, AudioEventType @event)
+        {
+            if (parentRack.Events.Find(e => e.EventType == @event) != null)
             {
-                InvokeEvent(@event, clip);
+                CustomAudioClip clip = caller as CustomAudioClip;
+                if (clip != null)
+                {
+                    InvokeEvent(@event, clip);
+                }
             }
         }
-    }
 
-    private void InvokeEvent(AudioEventType @event, CustomAudioClip clip)
-    {
-        switch (@event)
+        private void InvokeEvent(AudioEventType @event, CustomAudioClip clip)
         {
-            case AudioEventType.ClipBeginPlay:
-                ClipBeginPlay?.Invoke(clip);
-                break;
-            case AudioEventType.ClipPlayEnded:
-                ClipPlayEnded?.Invoke(clip);
-                break;
-                //case AudioEventType.ClipTempoIncrease:
-                //    ClipTempoIncrease?.Invoke(clip);
-                //    break;
-                //case AudioEventType.ClipTempoDecrease:
-                //    ClipTempoDecrease?.Invoke(clip);
-                //    break;
-                //case AudioEventType.ClipCustomTime:
-                //    ClipCustomTime?.Invoke(clip);
-                //    break;
+            switch (@event)
+            {
+                case AudioEventType.ClipBeginPlay:
+                    ClipBeginPlay?.Invoke(clip);
+                    break;
+                case AudioEventType.ClipPlayEnded:
+                    ClipPlayEnded?.Invoke(clip);
+                    break;
+                    //case AudioEventType.ClipTempoIncrease:
+                    //    ClipTempoIncrease?.Invoke(clip);
+                    //    break;
+                    //case AudioEventType.ClipTempoDecrease:
+                    //    ClipTempoDecrease?.Invoke(clip);
+                    //    break;
+                    //case AudioEventType.ClipCustomTime:
+                    //    ClipCustomTime?.Invoke(clip);
+                    //    break;
+            }
         }
     }
 }
